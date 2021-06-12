13572c824b7d0f53f9ac90686f1f93c30a7e06f0
Sergi Ortiz Gómez <drualcman@msn.com>
2021-06-12 10:59:31 +0800

    Session 1

---
+++ BlazingPizza/Client/Pages/Index.razor.cs
@@ -0,0 +1,13 @@
+﻿using BlazingPizza.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.Client.Pages
+{
+    public partial class Index
+    {
+        
+    }
+}
