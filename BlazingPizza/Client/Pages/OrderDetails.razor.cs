a84fa1f7782f47622f2161622f05118513189c19
Sergi Ortiz Gómez <drualcman@msn.com>
2021-07-10 14:55:39 +0800

    mostrar el estado de las ordenes

---
+++ BlazingPizza/Client/Pages/OrderDetails.razor.cs
@@ -0,0 +1,66 @@
+﻿using BlazingPizza.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.Client.Pages
+{
+    public partial class OrderDetails : IDisposable
+    {
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        [Parameter]
+        public int OrderId { get; set; }
+
+        OrderWithStatus OrderWithStatus;
+        bool InvalidOrder;
+
+        CancellationTokenSource PollingCancellatioinToken;
+
+        private async void PoolForUpdates()
+        {
+            PollingCancellatioinToken = new CancellationTokenSource();
+            while (!PollingCancellatioinToken.IsCancellationRequested)
+            {
+                try
+                {
+                    InvalidOrder = false;
+                    OrderWithStatus = await HttpClient.GetFromJsonAsync<OrderWithStatus>($"orders/{OrderId}");
+                    StateHasChanged();
+                    if (OrderWithStatus.IsDelivered)
+                    {
+                        PollingCancellatioinToken.Cancel();
+                    }
+                    else
+                    {
+                        await Task.Delay(4000);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    InvalidOrder = true;
+                    PollingCancellatioinToken.Cancel();
+                    Console.Error.WriteLine(ex.Message);
+                    StateHasChanged();
+                }
+            }
+        }
+
+        protected override void OnParametersSet()
+        {
+            PollingCancellatioinToken?.Cancel();
+            PoolForUpdates();
+        }
+
+        public void Dispose()
+        {
+            PollingCancellatioinToken?.Cancel();
+        }
+    }
+}
--- BlazingPizza/Server/Controllers/OrdersController.cs
+++ BlazingPizza/Server/Controllers/OrdersController.cs
@@ -2,6 +2,7 @@
 using BlazingPizza.Shared;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -43,6 +44,44 @@ namespace BlazingPizza.Server.Controllers
             await Context.SaveChangesAsync();
 
             return order.OrderId;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<OrderWithStatus>>> GetOrders()
+        {
+            var Orders = await Context.Orders
+                .Include(o => o.DeliveryLocation)
+                .Include(o => o.Pizzas).ThenInclude(p => p.Special)
+                .Include(o => o.Pizzas).ThenInclude(p => p.Toppings)
+                .ThenInclude(t => t.Topping)
+                .OrderByDescending(o=>o.CreatedTime)
+                .ToListAsync();
+            return Orders.Select(o=> OrderWithStatus.FromOrder(o)).ToList();
+        }
+
+        [HttpGet("{orderId}")]
+        public async Task<IActionResult> GetOrder(int orderId)
+        {
+            IActionResult result;
+
+            var Order = await Context.Orders
+                .Where(o => o.OrderId == orderId)
+                .Include(o => o.DeliveryLocation)
+                .Include(o => o.Pizzas).ThenInclude(p => p.Special)
+                .Include(o => o.Pizzas).ThenInclude(p => p.Toppings)
+                .ThenInclude(t => t.Topping)
+                .SingleOrDefaultAsync();
+
+            if (Order == null)
+            {
+                result = NotFound();
+            }
+            else
+            {
+                result = Ok(OrderWithStatus.FromOrder(Order));
+            }
+
+            return result;
         }
     }
 }
