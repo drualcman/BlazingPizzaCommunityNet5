4bf64746e2cb0008862175619bc75452b25be189
Sergi Ortiz Gómez <drualcman@msn.com>
2021-07-31 10:04:35 +0800

    preparar la interfaz de usuario para authenticacion

--- BlazingPizza/Client/Program.cs
+++ BlazingPizza/Client/Program.cs
@@ -21,6 +21,7 @@ namespace BlazingPizza.Client
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddScoped<OrderState>();
+            builder.Services.AddApiAuthorization();
 
             await builder.Build().RunAsync();
         }
